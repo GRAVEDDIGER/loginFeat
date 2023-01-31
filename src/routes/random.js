5ee510631e41f523295ec25930c2f550c5fe66fc
Adrian Abadin <aabadin@gmail.com>
2023-01-31 00:31:17 -0300

    Desafio Objeto Process y child

--- src/routes/random.js
+++ src/routes/random.js
@@ -1,5 +1,20 @@
 const router = require('express').Router()
+const {fork} = require('child_process')
 
-router.get('/:cant', randomNumber)
+router.get('/:cant', (req, res) => {
+    const randomChild = fork('./src/helper/randomNumbers.js')
+    randomChild.send(parseInt(req.params.cant))
+    randomChild.on('message', (numbers) => {
+        res.send(numbers)
+    })
+})
+
+router.get('/', (req, res) => {
+    const randomChild = fork('./src/helper/randomNumbers.js')
+    randomChild.send(100000000)
+    randomChild.on('message', (numbers) => {
+        res.send(numbers)
+    })
+})
 
 module.exports = router
