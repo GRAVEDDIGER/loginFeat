a55dbea9065ec4f4ee89781a31c18df5648b3bd5
Adrian Abadin <aabadin@gmail.com>
2023-02-07 04:25:37 -0300

    Working on clusters

--- src/helper/commandline.js
+++ src/helper/commandline.js
@@ -1,5 +1,7 @@
 const yargs = require('yargs/yargs')(process.argv.slice(2))
 let PORT
+let isFork = false
+let isCluster = false
 yargs.command(
 {    command:'port',
     describe:'Sets the port Configuration for the web server',
@@ -13,5 +15,19 @@ yargs.command(
     handler:(args) => {
       PORT = args.number
 }})
+
+yargs.command(
+    {    command:'FORK',
+        describe:'GENERATES A CHILD PROCESS AND EXECUTES THE SERVER IN IT ',
+        handler:(args) => {
+          isFork = true
+    }})
+yargs.command(
+        {    command:'CLUSTER',
+            describe:'GENERATES A A CHILD PROCESS FOR EACH CPU CORE',
+            handler:(args) => {
+              isCluster = true
+        }})
+
 yargs.parse()
-module.exports = PORT
+module.exports = {PORT, isFork, isCluster}
